1018bee0529a17dc85d33ace3fd54343eb74e5cf
Yann Zhang <yz.yannzhang@gmail.com>
2025-09-02 22:03:42 +0200

    bubble sort

---
+++ sorting_algorithms/bubble_sort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+
+void bubble_sort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i-1; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+void print_array(int arr[], int n)
+{
+    printf("[");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d", arr[i]);
+        if (i < n-1)
+        {
+            printf(",");
+
+        }
+    }
+    printf("]");
+}
+
+
+int main()
+{
+    int arr[] = {101, 21, 44, 32, 47, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Before sorting: ");
+    print_array(arr, n);
+
+    bubble_sort(arr, n);
+
+    printf("\nAfter bubble sorting: ");
+    print_array(arr, n);
+    printf("\n");
+}
